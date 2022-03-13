75a563546268edbd694c9a1c2068d5fb1fd3e45c
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-03-13 16:27:04 +0600

    done resposive

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -9,12 +9,12 @@ const List = (props) => {
     }
 
     return (
-        <div className='list-card col-md-12 col-12 gap-5'>
+        <div className='list-card col-md-12 gap-5'>
             <img src={src} alt="" />
             <div className='mx-2 container-fluid'>
-                <h2 className='container-fluid' >{title}</h2>
-                <h4 className='my-5'>{text1}</h4>
-                <h4>{text2}</h4> 
+                <h2 className='title' >{title}</h2>
+                <h4 className='text'>{text1}</h4>
+                <h4 className='text'>{text2}</h4> 
             </div>
             <button onClick={(e)=>handleDelete(e)} className='btn btn-danger delete-btn'>Delete</button>
         </div>
